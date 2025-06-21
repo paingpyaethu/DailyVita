63fbb679ceb5aada1fe70c4fda9878cf1b908db4
paingpyaethu <paingpyaethu@gmail.com>
2025-06-21 17:00:01 +0630

    Add Nunito font assets and theme configuration

---
+++ src/theme/colors.ts
@@ -0,0 +1,17 @@
+import {ColorSchemeName} from 'react-native';
+
+export const colors = {
+  primary: '#2f435d',
+  secondary: '#fe624e',
+  white: '#FFFFFF',
+  black: '#000000',
+  cyan: '#179ebc',
+  gray: '#808080',
+  lightGray: '#f4f4f4',
+  darkGray: '#505050',
+  background: '#d3f3e5',
+};
+
+export type Palette = (typeof colors)[keyof typeof colors];
+
+export type Theme = ColorSchemeName | keyof typeof colors;
